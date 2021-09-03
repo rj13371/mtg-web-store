3903f6c6ad0905168f47a37a6c171f42c60fa0f4
tblack10 <travis_black@live.com>
2021-09-02 19:02:15 -0700

    Connected DB to Atlas, some file restructuring

--- web-api/express/server.js
+++ web-api/express/server.js
@@ -1,11 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const cors = require('cors');
+const exp = require('express');
+const bp = require('body-parser');
+const { success, error } = require('consola')
+const { connect } = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('test')
-})
+// Bring in the app constants
+const { DB, PORT } = require('../config')
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// Initialize the application
+const app = exp();
+
+// Middlewares
+app.use(cors());
+app.use(bp.json());
+
+const startApp = async () => {
+    try {
+        // Connecion With DB
+        await connect(DB);
+        success({ 
+            message: `Successfully connected with the Database /n${DB}`, 
+            badge: true
+        });
+        app.listen(PORT, () => 
+            success({message: `Server started on PORT ${PORT}`, badge: true})
+        );
+    } catch(err) {
+        error({
+            message: `Unable to connected to with Database /n${error}`,
+            badge: true
+        })
+    }
+}
+
+startApp();
