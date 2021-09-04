f28b67e651da5fa57189204620499b17692bec7b
roland <rj1337@gmail.com>
2021-09-04 16:30:31 +0900

    added schemas

---
+++ web-api/express/models/MtgCard.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MtgCardSchema = new Schema({
+    cardGame: {
+        type: String,
+        required: true
+    }
+
+})
+
+// - cardGame: string
+//     - table: table
+//     - colors: arr
+//     - mana_cost: string
+//     - cmc: int,
+// - card_id: string
+// - set_id: string
+// - oracleText: string
+// - oracle_id: string (=product_id for MTG Cards)
+// - type_line: string
+// - imageURI obj: {small(string): url(string) ; medium(string): url(string)}
+// - stock: int
+// - price: int
+
+module.exports = mongoose.model('MtgCard', MtgCardSchema)
---
+++ web-api/express/models/Products.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    productName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    productDescription: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Double,
+        required: true,
+    },
+    productCategory: {
+        type: String,
+        required: true
+    },
+    onSale: {
+        type: Boolean
+    }
+})
+
+ // - _id: string
+//   - productname: string
+//   - stock:int
+//   - price:int 
+//   - productCatagory: string
+
+module.exports = mongoose.model('Product', ProductSchema)
