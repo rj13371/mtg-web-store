9bcfc5ff265233fcbb0d66e241d6b9accea0f07a
Philippczyck <fph.risingsunit@users.noreply.github.com>
2021-09-04 17:22:11 +0900

    Set up Schemas with Roland
    ・added different files for each cardgame and defined schemas
    ・(review required)

--- web-api/express/models/MtgCard.js
+++ web-api/express/models/MtgCard.js
@@ -2,11 +2,56 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MtgCardSchema = new Schema({
-    cardGame: {
+    // TODO: Copy Relevant Properties to Pokemon, YuGiOh Files
+    colors: {
+        // TODO: Research how enums are structured
+        type: String,
+        // JSON uses "", so we went with this, null is colorless
+        enum: [null, "W", "U", "B", "R", "G"],
+        default: null,
+        required: true
+    },
+    mana_cost: {
         type: String,
         required: true
+    },
+    cmc: {
+        type: Number,
+        required: true
+    },
+    set_id: {
+        type: String,
+        required: true,
+    },
+    oracleText: {
+        type: String,
+        required: true, 
+    },
+    type_line: {
+        // TODO: Is there a better way to structure this? Subtypes etc.
+        type: String,
+        required: true,
+    },
+    image_uris: {
+        type: String,
+        required: true,
+        unique: true,
+        size: [
+        // TODO: Figure out how to write nested properties
+        // the empty "" are supposed to point to different URLs
+            {"small": ""},
+            {"normal": ""},
+        ]
+    },
+    stock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    price: {
+        type: Number,
+        required: true
     }
-
 })
 
 // - cardGame: string
