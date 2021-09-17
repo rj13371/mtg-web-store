324904d74440211e8a3750d94e90e865e48a86ea
roland <rj1337@gmail.com>
2021-09-17 20:20:31 +0900

    revert

--- react/src/App.js
+++ react/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavbarComponent from './components/layouts/NavbarComponent';
 import CardSearch from './components/search/CardSearch';
 import Mtgcardsindex from './views/mtgcards/Mtgcardsindex';
+import EmployeeDashboard from './views/dashboard/EmployeeDashboard';
 
 import {
   Collapse,
@@ -28,6 +29,7 @@ function App() {
     <NavbarComponent/>
     <CardSearch/>
    <Route exact path='/cards/' render={(props) => (<Mtgcardsindex test="hi" {...props}/>)}/>
+   <Route exact path='/employeedashboard/' component={EmployeeDashboard}/>
 
   </Fragment>
 </Router>
