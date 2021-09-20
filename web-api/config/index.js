d98e57ef443d96785ae2e55e7692a8ad3ac64140
Travis <travisb.dev@gmail.com>
2021-09-20 14:40:13 -0700

    Update index.js

--- web-api/config/index.js
+++ web-api/config/index.js
@@ -4,5 +4,5 @@ require('dotenv').config({path:'../.env'})
 module.exports = {
     SECRET: process.env.APP_SECRET,
     DB: process.env.APP_DB,
-    PORT: process.env.APP_PORT
+    PORT: process.env.PORT
 }
