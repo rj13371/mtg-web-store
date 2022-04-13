9139288243545f728ffd578f707f150f8e9d7c66
rj1337 <mathesonroland@gmail.com>
2022-04-13 19:04:25 +0900

    make landing responsive

--- react/src/views/Landing.js
+++ react/src/views/Landing.js
@@ -1,7 +1,7 @@
-import React, { Fragment } from "react";
-import LandingCarousel from "../components/layouts/LandingCarousel";
-import { Container, Col, Row } from "react-bootstrap";
-import useWindowSize from "../hooks/useWindowSize";
+import React, { Fragment } from 'react';
+import LandingCarousel from '../components/layouts/LandingCarousel';
+import { Container, Col, Row } from 'react-bootstrap';
+import useWindowSize from '../hooks/useWindowSize';
 
 export default function Landing() {
   const size = useWindowSize();
@@ -9,19 +9,19 @@ export default function Landing() {
   return (
     <Fragment>
       <LandingCarousel
-        minWidth={"1300px"}
-        minHeight={"700px"}
-        maxWidth={"1500px"}
-        maxHeight={"700px"}
+        minWidth={'1300px'}
+        minHeight={'700px'}
+        maxWidth={'1500px'}
+        maxHeight={'700px'}
         carousel={0}
       />
 
-      <Container fluid style={{ padding: "10px" }}>
+      <Container fluid style={{ padding: '10px' }}>
         <Row>
           <Col lg>
             <iframe
               src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBastionGames%2F%3Fref%3Dpage_internal&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
-              width="370"
+              width={size.width > 500 ? size.width / 4.5 : 350}
               height="500"
               lazy="true"
               scrolling="no"
@@ -34,7 +34,7 @@ export default function Landing() {
           <Col lg>
             <iframe
               src="https://discord.com/widget?id=883211912731574344&theme=dark"
-              width="320"
+              width={size.width > 500 ? size.width / 4.5 : 350}
               height="490"
               allowtransparency="true"
               frameborder="2"
@@ -44,10 +44,10 @@ export default function Landing() {
 
           <Col lg>
             <LandingCarousel
-              minWidth={"666px"}
-              minHeight={"500px"}
-              maxWidth={"666px"}
-              maxHeight={"500px"}
+              minWidth={'666px'}
+              minHeight={'500px'}
+              maxWidth={'666px'}
+              maxHeight={'500px'}
               carousel={1}
             />
           </Col>
